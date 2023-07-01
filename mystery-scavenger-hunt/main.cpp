6414ed38533ad256dd554671d78c4775d11b3b89
CippoX <tommypalmi01@gmail.com>
2023-07-01 23:21:47 +0200

    Initial Commit

---
+++ mystery-scavenger-hunt/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  mystery-scavenger-hunt
+//
+//  Created by Tommaso Palmisano on 01/07/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
